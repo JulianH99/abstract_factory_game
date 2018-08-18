127bca17115709b308d9a3b83908e579b760917d
julianh99 <juliancorredors99@outlook.com>
2018-08-18 11:07:57 -0500

    Agregado un ligero ejemplo con socketio y agregago el archivo de requerimientos para python

--- server/main.py
+++ server/main.py
@@ -1,7 +1,20 @@
 from flask import Flask
+from flask_cors import CORS
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
+CORS(app)
+
+# get socket io working
+socketio = SocketIO(app)
 
 @app.route('/')
 def hello_world():
     return 'hello world'
+
+@socketio.on('connected')
+def connected():
+    print("Connected with sokectio from webpage")
+
+if __name__ == '__main__':
+    socketio.run(app)
--- server/run.sh
+++ server/run.sh
@@ -1,4 +1,5 @@
 export FLASK_APP=server/main.py
 export FLASK_ENV=development
+export FLASK_RUN_PORT=3001 
 
 flask run
