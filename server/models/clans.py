ca7352c603b9f56201b2287a3b926e3f8d13bcb3
Fabio <andresrenzas@gmail.com>
2018-08-28 16:59:01 -0500

    Agregados setters y getters

--- server/models/clans.py
+++ server/models/clans.py
@@ -12,6 +12,21 @@ class Clan(Sprite, metaclass=ABCMeta):
     def apply_attributes(self):
         pass
 
+    @property
+    def ships(self) -> List[ships]:
+        return self.__ships
+
+    @ships.setter
+    def name(self, new_ships: List[ships]):
+        self.__ships = new_ships
+
+    @property
+    def specialized_attributes(self) -> Dict[str, int]:
+        return self.__specialized_attributes
+
+    @specialized_attributes.setter
+    def name(self, new_specialized_attributes: Dict[str, int]):
+        self.__specialized_attributes = new_specialized_attributes
 
 class Kuirk(Clan):
     pass
--- server/models/ships.py
+++ server/models/ships.py
@@ -9,6 +9,38 @@ class Ship(Sprite, metaclass=ABCMeta):
     __attack: int
     __accessories: List[object]
 
+    @property
+    def speed(self) -> int:
+        return self.__speed
+
+    @speed.setter
+    def speed(self, new_speed: int):
+        self.__speed = new_speed
+
+    @property
+    def resistance(self) -> int:
+        return self.__resistance
+
+    @resistance.setter
+    def resistance(self, new_resistance: int):
+        self.__resistance = new_resistance
+
+    @property
+    def attack(self) -> int:
+        return self.__attack
+
+    @attack.setter
+    def attack(self, new_attack: int):
+        self.__resistance = new_attack
+
+    @property
+    def accesories(self) -> List[object]:
+        return self.__accesories
+
+    @accesories.setter
+    def accesories(self, new_accesories: List[object]):
+        self.__accesories = new_accesories
+
 
 class AgileShip(Ship):
     pass
